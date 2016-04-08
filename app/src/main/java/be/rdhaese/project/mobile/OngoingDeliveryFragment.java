ba87febce225465c1f01be87dc1c307e531396ea
Robin D'Haese <robin.d'haese@realdolmen.com>
2016-04-08 14:52:15 +0200

    injected necessary view components in fragment java class

--- app/src/main/java/be/rdhaese/project/mobile/OngoingDeliveryFragment.java
+++ app/src/main/java/be/rdhaese/project/mobile/OngoingDeliveryFragment.java
@@ -7,11 +7,44 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import roboguice.fragment.RoboFragment;
+import roboguice.inject.InjectView;
 
 
 public class OngoingDeliveryFragment extends RoboFragment {
+
+    @InjectView(R.id.txtPacketId)
+    private TextView txtPacketId;
+    @InjectView(R.id.txtCurrentPacketCount)
+    private TextView txtCurrentPacketCount;
+    @InjectView(R.id.txtPhoneNumber)
+    private TextView txtPhoneNumber;
+    @InjectView(R.id.txtStreet)
+    private TextView txtStreet;
+    @InjectView(R.id.txtNumber)
+    private TextView txtNumber;
+    @InjectView(R.id.txtMailbox)
+    private TextView txtMailbox;
+    @InjectView(R.id.txtPostalCode)
+    private TextView txtPostalCode;
+    @InjectView(R.id.txtCity)
+    private TextView txtCity;
+    
+    @InjectView(R.id.btnAddRemark)
+    private Button btnAddRemark;
+    @InjectView(R.id.btnCannotDeliver)
+    private Button btnCannotDeliver;
+    @InjectView(R.id.btnDeliver)
+    private Button btnDeliver;
+
+    @InjectView(R.id.switchConfirmVisually)
+    private Switch switchConfirmVisually;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
